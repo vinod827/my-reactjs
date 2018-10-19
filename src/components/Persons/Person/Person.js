4b22870dbaf4d1c22e52c05c395ee763a9f727c0
Vinod Kumar Nair <vinod827@gmail.com>
2018-10-19 21:33:45 +0530

    code updated to class components from functional components (persons, person)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
-  return (
-    <div className={classes.Person}>
-    <p onClick={props.click}>Im {props.name}</p>
-    <p>{props.children}</p>
-    <p>name is {props.name}</p>
-    <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-  );
-};
+class Person extends Component {
+  render(){
+    return (
+      <div className={classes.Person}>
+      <p onClick={this.props.click}>Im {this.props.name}</p>
+      <p>{this.props.children}</p>
+      <p>name is {this.props.name}</p>
+      <input type="text" onChange={this.props.changed} value={this.props.name}/>
+      </div>
+    );
+  }
+}
 
-export default person;
+export default Person;
